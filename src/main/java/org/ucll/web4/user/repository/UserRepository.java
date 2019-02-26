3e1b802d1134967a403efeffb2f3d47018c46548
Indy Naessens <indy.naessens@protonmail.com>
2019-02-26 18:32:37 +0100

    use annotations over concrete classes

--- src/main/java/org/ucll/web4/user/repository/UserRepository.java
+++ src/main/java/org/ucll/web4/user/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package org.ucll.web4.user.repository;
 
-import org.springframework.stereotype.Repository;
 import org.ucll.web4.GenericRepository;
 import org.ucll.web4.user.UserEntity;
 
 import java.util.UUID;
 
-@Repository
 public interface UserRepository extends GenericRepository<UserEntity, UUID> {
 
 }
